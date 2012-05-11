f6a7dc790592b24a11cba239ee1dd5c72b1b441b
Fred Loney <loneyf@ohsu.edu>
2012-05-11 10:27:41 -0700

    Put the user id in session scope.

--- lib/scat/authorization.rb
+++ lib/scat/authorization.rb
@@ -4,31 +4,40 @@ module Scat
   module Authorization
     include Sinatra::Authorization
     # Runs the given block in an HTTP basic authorization context.
+    # The session status is set to the result of performing the given block.
     #
-    # @yield the caTissue operation
+    # @yield perform the caTissue operation and return a status message
     def protect!(&block)
       @status = nil
       login_required
-      begin
-        CaTissue::Database.current.open(session[:email], session[:password], &block)
-      rescue
-        session[:status] = $!
-      end
+      session[:status] = perform!(session[:email], session[:password], &block)
     end
 
     # @return [CaTissue::User] the user who is submitting this edit
     # @raise [ArgumentError] if there is no such caTissue user
     def current_user
-      email = @login.first
+      # The caTissue login name is the user's email address.
+      email = session[:email]
+      raise Error.new("The caTissue login is not available in this web session.") unless email
+      # the current caTissue User
       user = CaTissue::User.new(:email_address => email)
-      user_id = @cache.get(:user, login)
+      # the cached caTissue User id
+      user_id = session[:user_id]
       if user_id then
         user.identifier = user_id
       else
+        # Fetch the User and cache the id.
         raise ArgumentError.new("User not found: #{user.email_address}") unless user.find
-        @cache.set(:user, login, user.identifier)
+        session[:user_id] = user_id
       end
       user
+    end
+      
+    # Fix +Sinatra::Authorization.authorization_realm+ per the
+    # (https://github.com/sr/sinatra-authorization)[https://github.com/sr/sinatra-authorization]
+    # patch.
+    def authorization_realm
+      settings.authorization_realm
     end
     
     private
@@ -37,13 +46,45 @@ module Scat
     # Formats the caTissue login name as _username_+@+_domain_,
     # where _domain_ is the domain portion of the system +hostname+.
     #
-    # @return [(String, String)] the caTissue login name and password
+    # @param [String] the caTissue login name, with or without the +@+_domain_ suffix
+    # @param [String] the caTissue password
+    # @return [Boolean] the authorization result
     def authorize(username, password)
-      domain = `hostname`[/\w+\.\w+$/]
-      session[:email] = [username, domain].join('@')
+      email = session[:email] = infer_email_address(username)
       session[:password] = password
-      logger.debug { "Scat captured caTissue login name #{@email} and password." }
-      true
+      # Try to start a session.
+      begin
+        CaTissue::Database.current.open(email, password) {}
+        logger.debug { "Scat captured the caTissue login name #{email} and password." }
+        true
+      rescue Exception => e
+        session[:status] = "caTissue login was unsuccessful - #{e.message}"
+        false 
+      end
+    end
+    
+    # Runs the given caTissue operation block with the given caTissue credentials.
+    #
+    # @yield (see #protect!)
+    # @return [String, nil] the status message
+    def perform!(email, password, &block)
+      login_required
+      begin
+        CaTissue::Database.current.open(email, password, &block)
+      rescue Exception => e
+        "Error encountered - #{e}" 
+      end
+    end
+    
+    # @param [String] the username, with or without the +@+_domain_ suffix
+    # @param [String] the email address
+    def infer_email_address(username)
+      if username.index('@') then
+        username
+      else 
+        domain = `hostname`.chomp.split('.')[1..-1]
+        domain.empty? ? username : [username, domain].join('@')
+      end
     end
   end
 end
--- lib/scat/specimen_edit.rb
+++ lib/scat/specimen_edit.rb
@@ -23,20 +23,26 @@ module Scat
     # Saves the +CaTissue::Specimen+ object specified in the request parameters.
     #
     # @param [{Symbol => String}] params the request parameters
+    # @return [String] the save status message
+    # @raise [ArgumentError] if the parameters do not include a +:protocol+
+    # @raise [CaTissue::DatabaseError] if the save is unsuccessful
     def save(params)
       validate(params)
       pcl = CaTissue::CollectionProtocol.new(:title => params[:protocol])
       raise ArgumentError.new("Protocol not found: #{pcl.title}") unless pcl.find
       cpe = pcl.events.first
       pnt = CaTissue::Participant.new(:last_name => params[:mrn])
-      mrn = CaTissue::ParticipantMedicalIdentifier.new(:participant => pnt, :medical_record_number => params[:mrn])
+      user = params[:user]
+      site = user.find.sites.first || CaTissue::Site.default_site
+      mrn = CaTissue::ParticipantMedicalIdentifier.new(:participant => pnt, :site => site, :medical_record_number => params[:mrn])
       reg = pcl.register(pnt)
       rqmt = cpe.requirements.first
       spc = CaTissue::Specimen.create_specimen(:requirement => rqmt, :initial_quantity => params[:quantity].to_f)
       scg = pcl.add_specimens(spc, :participant => pnt, :surgical_pathology_number => params[:spn],
-        :collection_event => cpe, :collector => current_user)
+        :collection_event => cpe, :collector => user)
       logger.debug { "Scat is saving the following specimen:\n#{spc.dump}" }
       spc.save
+      "Saved the patient #{mrn} specimen with label #{spc.label}."
     end
 
     private
