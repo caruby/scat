d832fe4276a6abb77a8dd43b8f293ec95ec8ba44
Fred Loney <loneyf@ohsu.edu>
2012-06-12 16:29:27 -0700

    Get CVs from a direct SQL query.

--- lib/scat/autocomplete.rb
+++ lib/scat/autocomplete.rb
@@ -1,3 +1,4 @@
+require 'jinx/helpers/inflector'
 require 'catissue/database/controlled_values'
 require 'scat/cache'
 
@@ -11,69 +12,63 @@ module Scat
     #
     # @param [Symbol] attribute the CV attribute
     # @param [String] prefix the leading value letters
-    # @return [<String>] the matching values
+    # @return [String] the JSON representation of the matching values
     def autocomplete(attribute, text)
       # Start up the cache if necessary
       @cache ||= Cache.new
-      # the match target string
-      tgt = text.downcase
-      # the first target word
-      word = tgt[/^\w+/]
+      # Compare lower case.
+      text_dc = text.downcase
+      # The search term is the first word in the text.
+      words = text_dc.split(' ')
+      term = words.first
+      logger.debug { "Scat is matching the cached #{attribute} values which contain #{term}..." }
+      # The hash key, e.g. dx:lymphoma.
+      key = KEY_PREFIX_HASH[attribute] + term
+      # The CVs which match the term.
+      cvs = @cache.get_all(key)
       # Load the CVs if necessary.
-      load_controlled_values(attribute) unless @cache.get(attribute)
-      # the CVs which matching the first word
-      cvs = @cache.get_all("#{attribute}:#{word}") || return
-      # the CVs which match the entire target
-      cvs.select { |cv| cv.downcase[tgt] }
+      cvs = load_controlled_values(attribute, term, key) if cvs.empty?
+      # The CVs which match the entire target.
+      matched = words.size == 1 ? cvs : cvs.select { |cv| cv.downcase[text_dc] }
+      logger.debug { "#{matched.empty? ? 'No' : matched.size} #{attribute} values match '#{text}'." }
+      matched.to_json
     end
     
     private
     
-    DIAGNOSES_STMT = "select value from catissue_permissible_value where public_id = 'Clinical_Diagnosis_PID'"
+    KEY_PREFIX_HASH = {
+       :clinical_diagnosis => 'dx:',
+       :tissue_site => 'ts:'
+    }
     
-    def load_controlled_values(attribute)
-      logger.debug { "Scat is loading the #{attribute} controlled values..." }
-      cnt = case attribute
-      when :clinical_diagnosis then
-        load_diagnoses
-      when :tissue_site then
-        load_tissue_sites
-      else
-        load_terminal_controlled_values(attribute)
-      end
-      logger.debug { "Scat loaded #{cnt} #{attribute} controlled values into the cache." }
-      @cache.set(attribute, true)
-    end
+    # The template to fetch CVs matching a PID and key term.
+    # The SQL cheats a little, since it theoretically could return a CV with null PID
+    # whose parentage is not directly or indirectly in the PID. However, this problem
+    # does not occur in practice and is relatively benign if it should occur, since at
+    # worst it includes a few obviously extraneous matches.
+    CV_TMPL = "select value
+      from catissue_permissible_value pv
+      where (public_id = '%s' or (public_id is null and parent_identifier is not null))
+      and value like '%%%s%%'
+      and not exists (select 1 from catissue_permissible_value where parent_identifier = pv.identifier)"
     
-    def load_diagnoses
-      cnt = 0
-      CaTissue::Database.current.executor.query(DIAGNOSES_STMT) do |rec|
+    # @param (see #match)
+    # @param [Strimg] the cache key
+    # @return (see #match)
+    def load_controlled_values(attribute, term, key)
+      logger.debug { "Scat is loading the #{attribute} controlled values which match '#{term}'..." }
+      # The CV PID, e.g. the :tissue_site PID is Tissue_Site_PID.
+      pid = attribute.to_s.split('_').map { |s| s.capitalize_first }.join('_') << '_PID'
+      # The SQL is the template formatted with the PID and term.
+      sql = CV_TMPL % [pid, term]
+      # Cache each fetched CV.
+      cvs = CaTissue::Database.current.executor.query(sql).map do |rec|
         cv = rec.first
-        cv.downcase.scan(/\w+/).each do |word|
-          if word.length > 4 then
-            @cache.add("clinical_diagnosis:#{word}", cv)
-          end
-        end
-        cnt += 1
+        @cache.add(key, cv)
+        cv
       end
-      cnt
-    end
-    
-    def load_terminal_controlled_values(attribute)
-      cnt = 0
-      cvs = CaTissue::ControlledValues.instance.for_public_id(attribute)
-      cvs.each do |cv|
-        next if cv.parent
-        val = cv.value
-        val.downcase.scan(/\w+/).each do |word|
-          if word.length > 4 then
-            @cache.add("#{attribute}:#{word}", val)
-          end
-        end
-        cnt += 1
-      end
-      CaTissue::ControlledValues.instance.clear
-      cnt
+      logger.debug { "Scat loaded #{cvs.size} #{attribute} #{term} controlled values." }
+      cvs
     end
   end
 end
